9948b9334ad458095d2d570c6cd65d4aeb74395c
DarkHarlock <b.sirianni@gmail.com>
2017-04-13 23:25:50 +0200

    Adds missing .gitignore (prevents commit with node_modules packages) Upgrades modules from CommonJs/Browser to UMD Adds and configure karma to allow testing also AMD modules

--- npm.js
+++ npm.js
@@ -1,13 +1,62 @@
-/* npm main module */
-'use strict';
-exports.LatLonSpherical   = require('./latlon-spherical.js');
-exports.LatLonEllipsoidal = require('./latlon-ellipsoidal.js');
-// merge vincenty methods into LatLonEllipsoidal
-var V = require('./latlon-vincenty.js');
-for (var prop in V) exports.LatLonEllipsoidal[prop] = V[prop];
-exports.LatLonVectors     = require('./latlon-vectors.js');
-exports.Vector3d          = require('./vector3d.js');
-exports.Utm               = require('./utm.js');
-exports.Mgrs              = require('./mgrs.js');
-exports.OsGridRef         = require('./osgridref.js');
-exports.Dms               = require('./dms.js');
+// Declares UMD module
+
+// eslint-disable-next-line strict
+(function (root, factory) {
+    // eslint-disable-next-line no-undef
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        // eslint-disable-next-line no-undef
+        define([
+            './latlon-spherical',
+            './latlon-vincenty',
+            './latlon-vectors',
+            './vector3d',
+            './utm',
+            './mgrs',
+            './osgridref',
+            './dms',
+        ], factory);
+    } else if (typeof module === 'object' && module.exports) {
+        // Node. Does not work with strict CommonJS, but
+        // only CommonJS-like environments that support module.exports,
+        // like Node.
+        module.exports = factory(
+            require('./latlon-spherical.js'),
+            require('./latlon-vincenty.js'),
+            require('./latlon-vectors.js'),
+            require('./vector3d.js'),
+            require('./utm.js'),
+            require('./mgrs.js'),
+            require('./osgridref.js'),
+            require('./dms.js')
+        );
+    } else {
+        // Browser globals (root is window)
+        root.Dms = factory();
+    }
+}(this, function (
+    LatLonSpherical,
+    LatLonEllipsoidal,
+    LatLonVectors,
+    Vector3d,
+    Utm,
+    Mgrs,
+    OsGridRef,
+    Dms) {
+    // eslint-disable-next-line strict
+    'use strict';
+
+    // Just return a value to define the module export.
+    // This example returns an object, but the module
+    // can return a function as the exported value.
+    return {
+        LatLonSpherical:   LatLonSpherical,
+        LatLonEllipsoidal: LatLonEllipsoidal,
+        LatLonVectors:     LatLonVectors,
+        Vector3d:          Vector3d,
+        Utm:               Utm,
+        Mgrs:              Mgrs,
+        OsGridRef:         OsGridRef,
+        Dms:               Dms,
+    };
+}));
--- test/latlon-vincenty-tests.js
+++ test/latlon-vincenty-tests.js
@@ -2,26 +2,43 @@
 /*  Geodesy Test Harness - latlon-vincenty                            (c) Chris Veness 2014-2016  */
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  */
 
-'use strict';
+// eslint-disable-next-line strict
+(function (root, factory) {
+    // eslint-disable-next-line no-undef
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        // eslint-disable-next-line no-undef
+        define([ 'chai', '../npm' ], factory);
+    } else if (typeof module === 'object' && module.exports) {
+        // Node. Does not work with strict CommonJS, but
+        // only CommonJS-like environments that support module.exports,
+        // like Node.
+        factory(require('chai'), require('../npm.js'));
+    } else {
+        // Browser globals (root is window)
+        root.Dms = factory();
+    }
+}(this, function (chai, npmjs) {
+    // eslint-disable-next-line strict
+    'use strict';
 
-var chai = require('chai');  // BDD/TDD assertion library
+    var LatLon = npmjs.LatLonEllipsoidal;
 
-var LatLon = require('../npm.js').LatLonEllipsoidal;
+    chai.should();
+    var test = it; // just an alias
 
-chai.should();
-var test = it; // just an alias
+    describe('latlon-vincenty', function() {
+        var le = new LatLon(50.06632, -5.71475), jog = new LatLon(58.64402, -3.07009);
+        test('vincenty inverse distance',              function() { le.distanceTo(jog).toFixed(3).should.equal('969954.166'); });
+        test('vincenty inverse initial bearing',       function() { le.initialBearingTo(jog).toFixed(4).should.equal('9.1419'); });
+        test('vincenty inverse final bearing',         function() { le.finalBearingTo(jog).toFixed(4).should.equal('11.2972'); });
 
-describe('latlon-vincenty', function() {
-    var le = new LatLon(50.06632, -5.71475), jog = new LatLon(58.64402, -3.07009);
-    test('vincenty inverse distance',              function() { le.distanceTo(jog).toFixed(3).should.equal('969954.166'); });
-    test('vincenty inverse initial bearing',       function() { le.initialBearingTo(jog).toFixed(4).should.equal('9.1419'); });
-    test('vincenty inverse final bearing',         function() { le.finalBearingTo(jog).toFixed(4).should.equal('11.2972'); });
+        var flindersPeak = new LatLon(-37.95103, 144.42487);
+        var buninyong = new LatLon(-37.6528, 143.9265);
+        test('vincenty direct destination',            function() { flindersPeak.destinationPoint(54972.271, 306.86816).toString('d').should.equal(buninyong.toString('d')); });
+        test('vincenty direct final brng',             function() { flindersPeak.finalBearingOn(54972.271, 306.86816).toFixed(4).should.equal('307.1736'); });
+        test('vincenty antipodal distance',            function() { new LatLon(0, 0).distanceTo(new LatLon(0.5, 179.5)).should.equal(19936288.579); });
 
-    var flindersPeak = new LatLon(-37.95103, 144.42487);
-    var buninyong = new LatLon(-37.6528, 143.9265);
-    test('vincenty direct destination',            function() { flindersPeak.destinationPoint(54972.271, 306.86816).toString('d').should.equal(buninyong.toString('d')); });
-    test('vincenty direct final brng',             function() { flindersPeak.finalBearingOn(54972.271, 306.86816).toFixed(4).should.equal('307.1736'); });
-    test('vincenty antipodal distance',            function() { new LatLon(0, 0).distanceTo(new LatLon(0.5, 179.5)).should.equal(19936288.579); });
-
-    test('vincenty antipodal convergence failure', function() { new LatLon(0, 0).distanceTo(new LatLon(0.5, 179.7)).should.be.NaN; });
-});
+        test('vincenty antipodal convergence failure', function() { new LatLon(0, 0).distanceTo(new LatLon(0.5, 179.7)).should.be.NaN; });
+    });
+}));
